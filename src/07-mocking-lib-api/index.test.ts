08b238d3683f8c3104cdf3919ff46756acd4e028
marygrishchuk <mary.grishchuk1@gmail.com>
2026-04-20 02:21:31 +0300

    test: mock library api

--- src/07-mocking-lib-api/index.test.ts
+++ src/07-mocking-lib-api/index.test.ts
@@ -1,17 +1,67 @@
-// Uncomment the code below and write your tests
-/* import axios from 'axios';
-import { throttledGetDataFromApi } from './index'; */
+jest.mock('axios', () => {
+  return {
+    __esModule: true,
+    default: {
+      create: jest.fn(),
+    },
+  };
+});
+
+jest.mock('lodash', () => {
+  const originalModule = jest.requireActual<typeof import('lodash')>('lodash');
+
+  return {
+    __esModule: true,
+    ...originalModule,
+    throttle: jest.fn((functionToThrottle) => functionToThrottle),
+  };
+});
+
+import axios from 'axios';
+
+import { throttledGetDataFromApi } from './index';
 
 describe('throttledGetDataFromApi', () => {
+  const RELATIVE_PATH = '/todos/1';
+  const RESPONSE_DATA = { id: 1, title: 'mocked todo' };
+  const BASE_URL = 'https://jsonplaceholder.typicode.com';
+
+  function buildAxiosInstance(getSpy: jest.Mock) {
+    return {
+      get: getSpy,
+    } as unknown as ReturnType<typeof axios.create>;
+  }
+
   test('should create instance with provided base url', async () => {
-    // Write your test here
+    const getSpy = jest.fn().mockResolvedValue({ data: RESPONSE_DATA });
+    const createSpy = jest.mocked(axios.create);
+
+    createSpy.mockReturnValue(buildAxiosInstance(getSpy));
+
+    await throttledGetDataFromApi(RELATIVE_PATH);
+
+    expect(createSpy).toHaveBeenCalledWith({
+      baseURL: BASE_URL,
+    });
   });
 
   test('should perform request to correct provided url', async () => {
-    // Write your test here
+    const getSpy = jest.fn().mockResolvedValue({ data: RESPONSE_DATA });
+
+    jest.mocked(axios.create).mockReturnValue(buildAxiosInstance(getSpy));
+
+    await throttledGetDataFromApi(RELATIVE_PATH);
+
+    expect(getSpy).toHaveBeenCalledWith(RELATIVE_PATH);
   });
 
   test('should return response data', async () => {
-    // Write your test here
+    const getSpy = jest.fn().mockResolvedValue({ data: RESPONSE_DATA });
+
+    jest.mocked(axios.create).mockReturnValue(buildAxiosInstance(getSpy));
+
+    await expect(throttledGetDataFromApi(RELATIVE_PATH)).resolves.toEqual(
+      RESPONSE_DATA,
+    );
   });
 });
